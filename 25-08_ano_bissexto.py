164a8962941d13ebe0b04289670b76c81ccba8b4
FilipeGamer12 <74568043+FilipeGamer12@users.noreply.github.com>
2025-08-25 19:49:59 -0300

    ano bissexto v1

---
+++ 25-08_ano_bissexto.py
@@ -0,0 +1,8 @@
+ano_max = int(input("Digite o ano máximo (até 4000): "))
+contador = 0
+
+for ano in range(2000, ano_max + 1):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        contador += 1
+
+print("Quantidade de anos bissextos:", contador)
