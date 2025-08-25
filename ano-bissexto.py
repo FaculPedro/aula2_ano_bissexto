b5d578376ff596ee1f019cb5176dc4d31a696ade
FaculPedro <172211008@ulife.com.br>
2025-08-25 20:28:45 -0300

    [FIX] adicionar ano maximo na valição de ano

--- ano-bissexto.py
+++ ano-bissexto.py
@@ -1,17 +1,17 @@
-def eh_bissexto(ano):
-    return (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0)
+def is_leap_year(year):
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-ano_final = int(input("Digite um ano final (maior ou igual a 2000): "))
+input_year = int(input("Digite um ano final (maior ou igual a 2000): "))
 
-def valida_ano():
-  return ano_final < 2000
+def validate_year():
+    return input_year <= 4000 and input_year > 2000  
 
-if valida_ano():
+if not validate_year():
     print("O ano deve ser 2000 ou maior.")
 else:
-    bissextos = 0
-    for ano in range(2000, ano_final + 1):
-        if eh_bissexto(ano):
-            bissextos += 1
+    leap_years = 0
+    for year in range(2000, input_year + 1):
+        if is_leap_year(year):
+            leap_years += 1
 
-    print(f"De 2000 até {ano_final}, existem {bissextos} anos bissextos.")
+    print(f"De 2000 até {input_year}, existem {leap_years} anos bissextos.")
